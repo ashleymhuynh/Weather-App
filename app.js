3e69a2236147b880c5273636062bc0fb0b3ff034
ashleymhuynh <ashley.huynh23@gmail.com>
2021-06-09 11:53:58 -0400

    add weather description

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
+//create a function to display current time
 function getDate() {
   let now = new Date();
   let hours = now.getHours();
   let minutes = now.getMinutes();
-
   let dayIndex = now.getDay();
   let days = [
     "Sunday",
@@ -14,7 +14,6 @@ function getDate() {
     "Saturday",
   ];
   let day = days[dayIndex];
-
   if (hours < 10) {
     hours = `0${hours}`;
   }
@@ -27,5 +26,37 @@ function getDate() {
 let currentTime = document.querySelector("#currentdate");
 currentTime.innerHTML = getDate();
 
+function searchCity(event) {
+  event.preventDefault();
+  let inputCity = document.querySelector("#searched-city");
+  let currentCity = document.querySelector("#current-city");
+  //display city name
+  currentCity.innerHTML = inputCity.value;
+  fetchData(inputCity.value);
+  //cler input value
+  inputCity.value = "";
+}
+
+async function fetchData(inputCity) {
+  const apiKey = "650fb9db077f61ca16fc2d3df93a734e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&appid=${apiKey}&units=imperial`;
+  try {
+    const response = await axios.get(apiUrl);
+    const cityInfo = response.data;
+    console.log(cityInfo);
+    displayTemperature(cityInfo);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+function displayTemperature(cityInfo) {
+  let weatherDescriptionElement = document.getElementById(
+    "weather-description"
+  );
+  let weatherDescription = cityInfo.weather[0].description;
+  weatherDescriptionElement.innerHTML = weatherDescription;
+}
+
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
+searchForm.addEventListener("submit", searchCity);
